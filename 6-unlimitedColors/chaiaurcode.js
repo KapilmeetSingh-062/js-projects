a62f90c95ad9035d21b2691008afc9f44b4acfaf
KapilmeetSingh-062 <kapilmeetbaath@gmail.com>
2023-12-21 16:55:02 -0700

    Unlimited colors project completed

---
+++ 6-unlimitedColors/chaiaurcode.js
@@ -0,0 +1,32 @@
+// Generate a random color with Hex values
+const randomColor = function () {
+    hexValues = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += hexValues[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  
+  let intervalId;
+  const startChangingColor = function () {
+    if (!intervalId) {
+      intervalId = setInterval(changingBgColor, 1000);
+    }
+  
+    function changingBgColor() {
+      document.body.style.backgroundColor = randomColor();
+    }
+  };
+  
+  const stopChangingColor = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+  };
+  
+  const startBtn = document.querySelector('#start');
+  startBtn.addEventListener('click', startChangingColor);
+  
+  const stopBtn = document.querySelector('#stop');
+  stopBtn.addEventListener('click', stopChangingColor);
+  
